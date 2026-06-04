d37c204bb66f4e0a39fe862457d8f118a59d8b1e
1 <you@example.com>
2026-06-04 10:14:50 +0700

    04062026

--- js/lib.js
+++ js/lib.js
@@ -1,3 +1,39 @@
-body{
-	background-color: Blue;
+/*Viết mã Javascript*/
+function printBCC(num)
+{
+    let result = "";
+
+    let i = 1;
+    while(i<=10)
+    {
+        result += `${num} x ${i} = ${num*i} <br>`; 
+        i++;
+    }
+    
+    //Đưa kết quả vào thẻ div có id là "result"
+    document.getElementById("result").innerHTML = result;
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
